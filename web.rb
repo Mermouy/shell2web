b2d012806d4890ceca9101c1597681cb58404f4f
Barry Allard <barry.allard@gmail.com>
2014-01-08 06:50:11 -0800

    initial

---
+++ web.rb
@@ -0,0 +1,87 @@
+require 'sinatra'
+# std
+require 'fileutils'
+require 'open3'
+
+
+TIME=true
+CMD='./run'
+OUTPUT='tmp/output.txt'
+OUTPUT_TMP=OUTPUT+$$.to_s
+WAIT_TIME=5*60 # 5 minutes
+
+DAYS_PER_YEAR = 365.242199
+DAYS_PER_MONTH = DAYS_PER_YEAR / 12
+SECONDS_PER_DAY = 86400.0
+
+def seconds_to_english(sec, sep=' ')
+  def plural(x, word)
+    "#{x} #{(x >= 1.0 && x < 2.0) ? word : word+'s'}"
+  end
+  ({
+    'year'   => DAYS_PER_YEAR*SECONDS_PER_DAY,
+    'month'  => DAYS_PER_MONTH*SECONDS_PER_DAY,
+    'week'   => 7*SECONDS_PER_DAY,
+    'day'    => SECONDS_PER_DAY,
+    'hour'   => 3600.0,
+    'minute' => 60.0 }.map { |unit, unit_divisor|
+      if (x = (sec / unit_divisor).floor) >= 1.0
+        sec -= x * unit_divisor
+        plural(x, unit)
+      end
+  }.compact << plural(sec, 'second')).join(sep)
+end
+
+def run(f) 
+  if TIME
+    start_date = Time.now
+    f << "# started: #{start_date}\n"
+  end
+
+  Open3.popen2e(CMD) { |stdin, stdout_and_stderr, wait_thr| 
+    stdout_and_stderr.each {|line|
+      f << line
+      $stderr.puts("read line: #{line}")
+    }
+    exit_status = wait_thr.value.to_i
+  }
+  last = "exit code: #{exit_status}"
+  if TIME
+    end_date = Time.now
+    elapsed_seconds = end_date - start_date
+    last = "finished: #{end_date}   elapsed: #{seconds_to_english(elapsed_seconds)}  " + last
+  end
+  f << "# #{last}\n"
+end
+
+Thread.new do
+  loop do 
+    $stderr.puts "Waiting"
+    sleep(WAIT_TIME)
+    $stderr.puts "Running"
+    exit_status = -1
+    File.open(OUTPUT_TMP, 'w') {|f| run(f) }
+    FileUtils.mv OUTPUT_TMP, OUTPUT, :force => true
+    $stderr.puts "Done"
+  end
+end if false
+
+before do
+  content_type 'text/plain'
+end
+
+get '/live' do
+  stream do |f|
+    run(f)
+  end
+end 
+
+get '/' do
+  result = ''
+  File.open(OUTPUT, 'r') { |f|
+    while line = f.gets
+      result += (line =~ /\n$/) ? (line.chop + "\r\n") : line
+    end
+  } if File.exist? OUTPUT
+  result
+end
