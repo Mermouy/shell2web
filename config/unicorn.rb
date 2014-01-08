252e6f2fcfc7d39dde549cb12f7eccc3d317751f
Barry Allard <barry.allard@gmail.com>
2014-01-08 14:54:19 -0800

    support very slow scripts

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,21 @@
 # config/unicorn.rb
-worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
-timeout Integer(ENV['WEB_TIMEOUT'] || 300)
+worker_processes Integer(ENV['SHELL2WEB_CONCURRENCY'] || 3)
+timeout Integer(ENV['SHELL2WEB_TIMEOUT'] || 1200)
 preload_app true
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    $stderr.puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  ActiveRecord::Base.connection.disconnect! if defined?ActiveRecord::Base
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    $stderr.puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection if defined? ActiveRecord::Base
 end
